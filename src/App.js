a2e224f349bb9dd916ba702106a9d328c6a4f2fa
Marie Preti <marielapreti@gmail.com>
2022-07-03 16:02:43 -0300

    update App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import ItemCount from "./components/ItemCount/ItemCount";
+
 import "./app.css";
 import ItemDetailContainer from "./components/ItemDetailContainer";
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
-function App() {
+
+function App() {  
   
-  function handleSubmit(evt){
-        evt.preventDefault()
-      ;
-      console.log ('handler button');
-
-      }
-  
-  function   handleKeyDown(evt) {
-    const arrayVocales = 'aeiou'.split('');    
-
-    console.log (evt.key);
-    const tecla = evt.key;
-     if (arrayVocales.includes(evt.key))
-      evt.preventDefault();
-    
-  }
-
   return (
     <div className="App"> 
 
@@ -35,17 +19,13 @@ function App() {
                 
             <Route path="/item/:id" element={<ItemDetailContainer greet= "Detalle del producto"/>}></Route>          
              
-            <Route path="/counting" element={<ItemCount stock={5} initial={1}/>}></Route>  
+           
 
             <Route path="/category/:categoryId" element={<ItemListContainer greet= "Categoria de Items"/>}></Route>    
         </Routes>
      </BrowserRouter>
 
-     <form>
-      <input onHeyDown ={handleKeyDown}
-      placeholder = "hola"></input>
-      <button onClick={handleSubmit}>SUBMIT</button>
-     </form>
+     
 
     </div>
   );
